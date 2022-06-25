f20a5d4959013b550c649fa1b99a4353e1daae88
vladwarrior <vladislav.mineev.07@mail.ru>
2022-06-25 16:13:54 +0300

    try change gendiff while

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -6,5 +6,6 @@ namespace Differ\Formatters\Json;
 
 function format(array $tree): string
 {
+    //echo json_encode($tree) . "lkjhnbvcsdcfsrdtyujtyhgbfvcdsacsvfghn";
     return json_encode($tree);
 }
